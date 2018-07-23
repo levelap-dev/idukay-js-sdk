946ee725317878d6bd0b55020e213622567bbcac
Angie Rojas <nataliart4@gmail.com>
2018-07-23 10:41:20 -0500

    COL-2361, fixed test create post

--- src/idukay/specs/login.spec.js
+++ src/idukay/specs/login.spec.js
@@ -1,16 +1,27 @@
 import login from 'idukay/login';
 import {createPost} from 'idukay/login';
 
+let write, getElementById, addEventListener;
+const mockEventListener = {
+  addEventListener: jest.fn()
+}
 
 describe('When login', () => {
   beforeEach(() => {
-    spyOn(window, 'open');
-    spyOn(document, 'write');
+    write = jest.fn();
+    addEventListener = mockEventListener;
+    getElementById = jest.fn(() => {
+      return addEventListener
+    });
+
+    spyOn(window, 'open').and.returnValue({document: {write, getElementById}});
+
     fetch.resetMocks();
     fetch.mockResponseOnce({user: 'user'});
   });
   describe('When create post', () => {
     it('should post', async () => {
+      login();
       const user =  await createPost();
       expect(user.body).toEqual({user:'user'});
     });
