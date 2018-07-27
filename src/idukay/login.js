58af525627964deb037259b08f00f9c174961664
Angie Rojas <nataliart4@gmail.com>
2018-07-27 12:16:36 -0500

    COL-2361, modified login and created is user logged fn, created tests

--- src/idukay/login.js
+++ src/idukay/login.js
@@ -1,57 +1,75 @@
 let popUp;
 let email;
 let password;
-let promise;
+let user_promise;
 
-const createPost = async () => {
-  email = popUp.document.getElementById("email").value;
-  password = popUp.document.getElementById("password").value;
+const createPost =  (data, callback) => {
+  return async () => {
+    email = popUp.document.getElementById("email").value;
+    password = popUp.document.getElementById("password").value;
 
-  let response = await fetch('http://localhost:3001/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: `email=${email}&password=${password}`
-  });
+    let response = await fetch('http://dev.idukay.net/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `email=${email}&password=${password}`
+    });
 
-  console.log('JSON:::',response.json());
-  promise = response.json();
+    user_promise = response.json()
+    .then((resp) => {
+      if (resp.errors.length) {
+        popUp.close();
+        callback(resp.errors);
+      }
+
+      if (Object.keys(resp.response).length) {
+        localStorage.setItem('idukaysdk/token', resp.response.token);
+        popUp.close();
+        callback(null, {user: {email: resp.response.user.email, school: resp.response.user.school, id: resp.response.user._id}});
+      }
+    })
+    .catch((error) => {
+      popUp.close();
+      throw (error);
+    });
+  };
 };
 
 
-const getLoggedUser = () => {
-  promise.then((resp) => {
-    console.log('getLogged::',resp);
-  });
+const isUserLogged = () => {
+  return localStorage.getItem('idukaysdk/token') ? true : false;
 };
 
-const login = () => {
+const login = (data, callback) => {
+  localStorage.removeItem('idukaysdk/token');
 
-  const html = '<div>'+
-  '<form>'+
-  '<div>'+
-  '<div>'+
-  '<h2 style="color:green;">Login con Idukay</h2>'+
-  '<div>&nbsp;</div>'+
-  '<span> <em>&nbsp;</em> <input id="email" name="email" required="" type="email" placeholder="Correo '+ 'electr&oacute;nico" /> </span></div>'+
-  '</div>'+
-  '<div>'+
-  '<div><span> <em>&nbsp;</em> <input id="password" name="password" required="" type="password"'+ 'placeholder="Contrase&ntilde;a" /> </span></div>'+
-  '</div>'+
-  '<div>&nbsp;</div>'+
-  '<div><button id="iduButton" type="button"}>Ingresar</button></div>'+
-  '</form></div>';
+  let html = `<div>
+  <form>
+  <div>
+  <div>
+  <h2 style="color:green;">Login con Idukay</h2>
+  <div>&nbsp;</div>
+  <span> <em>&nbsp;</em> <input id="email" name="email" required="" type="email" placeholder="Correo  electr&oacute;nico" /> </span></div>
+  </div>
+  <div>
+  <div><span> <em>&nbsp;</em> <input id="password" name="password" required="" type="password" placeholder="Contrase&ntilde;a" /> </span></div>
+  </div>
+  <div>&nbsp;</div>
+  <div><button id="iduButton" type="button"}>Ingresar</button></div>
+  </form>
+  </div>`;
 
   popUp = window.open('','Login', 'width=400, height=400');
   popUp.document.write('');
   popUp.document.write(html);
-  popUp.document.getElementById("iduButton").addEventListener("click", createPost);
+  popUp.document.getElementById("iduButton").addEventListener("click", createPost(data, callback));
 
+  return user_promise;
 };
 
 export {createPost};
 
-const idukay = {login, getLoggedUser};
+const idukay = {login, isUserLogged};
 
 export default idukay;
--- src/idukay/specs/login.spec.js
+++ src/idukay/specs/login.spec.js
@@ -1,53 +1,121 @@
-import login from 'idukay/login';
+import idukay from 'idukay/login';
 import {createPost} from 'idukay/login';
+import 'jest-localstorage-mock';
 
-let write, getElementById, addEventListener;
+let write, close, getElementById, addEventListener, removeItem;
+
 const mockEventListener = {
   addEventListener: jest.fn()
-}
+};
 
 describe('When login', () => {
   beforeEach(() => {
     write = jest.fn();
+    close = jest.fn();
     addEventListener = mockEventListener;
+
+    removeItem = jest.fn();
+
     getElementById = jest.fn(() => {
       return addEventListener
     });
 
-    spyOn(window, 'open').and.returnValue({document: {write, getElementById}});
+    spyOn(window, 'open').and.returnValue({document: {write, getElementById}, close});
 
     fetch.resetMocks();
-    fetch.mockResponseOnce({user: 'user'});
   });
-  describe('When create post', () => {
-    it('should post', async () => {
-      login();
-      const user =  await createPost();
-      expect(user.body).toEqual({user:'user'});
+
+  describe('when calling login fn', () => {
+    it('should return a promise', (done) => {
+      let expected_promise = Promise.resolve({user: 'user'});
+      let callback = jest.fn();
+
+      spyOn(idukay, 'login').and.returnValue(expected_promise);
+
+      idukay.login({}, callback).then((user) => {
+        expect(user).toEqual({user: 'user'});
+        done();
+      });
+    });
+
+    it('should call localStorage.removeItem', () => {
+      idukay.login({}, jest.fn);
+      expect(localStorage.removeItem).toHaveBeenLastCalledWith('idukaysdk/token');
     });
   });
-  //describe('When create post', () => {
-  //  fit('should post', async () => {
-  //    login();
-  //    const user =  await createPost();
-  //    console.log('expect:::', user);
-  //    //expect(user.body).toEqual({user:'user'});
-  //  });
-  //
-  //  //fit('should post', () => {
-  //  //  login();
-  //  //  createPost().then((resss) => {
-  //  //
-  //  //    console.log('expect:::', resss);
-  //  //  });
-  //  //
-  //  //  //expect(user.body).toEqual({user:'user'});
-  //  //});
-  //});
-  //describe('When getting logged user', () => {
-  //  it('should return logged user if exists', () => {
-  //    const logged_user = await getLoggedUser();
-  //    expect(logged_user._id).toBe('12345');
-  //  });
-  //});
+
+  describe('when calling create post fn', () => {
+    it('should get correct response on callback and call localStorage.setItem if not exists errors', async (done) => {
+      fetch.mockResponseOnce(JSON.stringify({errors: [], response: {token:'123', user: {_id: 'id_user', name: 'name', email: 'email', school: 'school'}}}));
+
+      let callback = function (error, resp) {
+        if (error) {
+          done.fail('should not fail');
+        }
+
+        if(resp) {
+          expect(localStorage.setItem).toHaveBeenLastCalledWith('idukaysdk/token', '123');
+          expect(resp).toEqual({user: {email: 'email', school: 'school', id: 'id_user'}});
+          done();
+        }
+      };
+
+      idukay.login({}, callback);
+      await createPost({}, callback)();
+    });
+
+    it('should get correct response on callback if not exists errors', async (done) => {
+      fetch.mockResponseOnce(JSON.stringify({errors: ['some_errors'], response: {}}));
+
+      let callback = function (errors, resp) {
+        if (errors) {
+          expect(errors[0]).toBe('some_errors');
+          done();
+        }
+
+        if(resp) {
+          done.fail('should not fail');
+        }
+      };
+
+      idukay.login({}, callback);
+      await createPost({}, callback)();
+    });
+
+    it('should catch other type of error and throw them', async (done) => {
+      fetch.mockRejectOnce('catch_errors');
+
+      let callback = function (errors, resp) {
+        if (errors) {
+          done.fail('should not fail');
+        }
+
+        if(resp) {
+          done.fail('should not fail');
+        }
+      };
+
+      idukay.login({}, callback);
+      await createPost({}, callback)()
+      .catch((errors) => {
+        expect(errors).toBe('catch_errors');
+        done();
+      });
+    });
+  });
+
+  describe('when calling is user logged fn', () => {
+    beforeEach(() => {
+      localStorage.clear();
+    });
+
+    it('should return true if idukaysdk/token exists ', () => {
+      localStorage.setItem('idukaysdk/token', 'my_token');
+      expect(idukay.isUserLogged()).toBe(true);
+    });
+
+    it('should return false if idukaysdk/token not exists', () => {
+      expect(idukay.isUserLogged()).toBe(false);
+    });
+  });
 });
